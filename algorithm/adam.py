6f5380496b1dec98554172ab9443895694626d00
fififionaz <sunsetave.bunny@gmail.com>
2018-05-09 23:18:09 -0700

    fixing inf

--- algorithm/adam.py
+++ algorithm/adam.py
@@ -9,31 +9,32 @@ beta_1 = 0.9
 beta_2 = 0.999
 epsilon = 1e-8
 
-theta_0 = 0
-m_t = 0 
-v_t = 0 
-t = 0
+# def func(A, x):
+#     return A.dot(x)
 
-def func(A, x):
-    return A.dot(x)
+# def grad_func(A, x):#calculates the gradient
+#     return A
 
-def grad_func(A, x):#calculates the gradient
-    return A
-
-def adam(A, x, b, lr = 1e-4, tol = 1e-5, nmax = 1e6):
-    r = b-A.dot(x)
+def adam(obj, grad, x, A, lr = 1e-4, tol = 1e-5, nmax = 1e6):
+    theta_0 = 0
+    m_t = 0 
+    v_t = 0 
     iter_num = 0
-    res = [linalg.norm(r)/linalg.norm(b)]
+
+    r = obj(x)
+    iter_num = 0
+    res = [linalg.norm(r)]
     while iter_num < nmax:
         if res[iter_num] < tol:
             break
         iter_num+=1
-        g_t = grad_func(A, theta_0)
+        g_t = grad(x)
         m_t = beta_1*m_t + (1-beta_1)*g_t
         v_t = beta_2*v_t + (1-beta_2)*(g_t*g_t)
-        m_cap = m_t/(1-(beta_1**t))
-        v_cap = v_t/(1-(beta_2**t))
+        m_cap = m_t/(1-(beta_1**iter_num))
+        v_cap = v_t/(1-(beta_2**iter_num))
         theta_0_prev = theta_0
+        print(m_cap)
         theta_0 = theta_0 - (alpha*m_cap)/(math.sqrt(v_cap)+epsilon)
         print(theta_0)
         if(theta_0 == theta_0_prev):
